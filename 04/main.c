54b8f810664a2561857c8248e8a31e9bac8030e0
douglas engstrand <doueng09@gmail.com>
2019-06-19 22:27:20 +0200

    reset keyboard alarm

--- 04/main.c
+++ 04/main.c
@@ -4,12 +4,36 @@
 #include <linux/hid.h>
 #include <linux/usb.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Douglas Engstrand");
-MODULE_DESCRIPTION("Keyboard alarm system");
+static void usb_kbd_disconnect(struct usb_interface *intf)
+{
+	pr_info("KEYBOARD IS GONE!!!");
+}
 
-static struct usb_driver kbd_driver = {
-	.name = "keyboard alarm",
+static int usb_kbd_probe(struct usb_interface *iface,
+			 const struct usb_device_id *id)
+{
+	pr_info("KEYBOARD DETECTED");
+	return (0);
+}
+
+static const struct usb_device_id usb_kbd_id_table[] = {
+	{ USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
+						 USB_INTERFACE_SUBCLASS_BOOT,
+						 USB_INTERFACE_PROTOCOL_KEYBOARD) },
+	{ }						/* Terminating entry */
+};
+
+MODULE_DEVICE_TABLE (usb, usb_kbd_id_table);
+
+static struct usb_driver usb_kbd_driver = {
+	.name =			"keyboard alarm",
+	.probe =		usb_kbd_probe,
+	.disconnect =	usb_kbd_disconnect,
+	.id_table =		usb_kbd_id_table,
 };
 
 module_usb_driver(kbd_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Douglas Engstrand");
+MODULE_DESCRIPTION("Keyboard alarm system");
