8b7924607a350e782e0841f57c89639eb388e109
doueng <doueng09@gmail.com>
2019-06-22 19:23:23 +0200

    b

--- 04/keyboard_alarm.c
+++ 04/keyboard_alarm.c
@@ -9,7 +9,7 @@ static void usb_kbd_disconnect(struct usb_interface *intf)
 	pr_info("KEYBOARD IS GONE!!!");
 }
 
-static int usb_kbd_probe(struct usb_interface *iface,
+static int usb_kbd_probe(struct usb_interface *intf,
 			 const struct usb_device_id *id)
 {
 	pr_info("KEYBOARD DETECTED");
@@ -25,8 +25,8 @@ static const struct usb_device_id usb_kbd_id_table[] = {
 
 MODULE_DEVICE_TABLE(usb, usb_kbd_id_table);
 
-static struct usb_driver usb_kbd_driver = {
-	.name = "keyboard alarm",
+static struct usb_driver kbd_driver = {
+	.name = "keyboard_alarm",
 	.probe = usb_kbd_probe,
 	.disconnect = usb_kbd_disconnect,
 	.id_table = usb_kbd_id_table,
